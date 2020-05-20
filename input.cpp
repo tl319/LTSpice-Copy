12c83445153414e08e48b8d60c4fa8582c103bcb
jyt19 <jyt19@imperial.ac.uk>
2020-05-20 15:56:36 -0700

    created input reader

---
+++ input.cpp
@@ -0,0 +1,71 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+struct Component
+{
+	char type;
+	string name;
+	string nA;
+	string nB;
+	float value;
+};
+
+bool isComponent(string x)
+{
+	if(isalpha(x[0])){
+		return true;}
+	return false;
+}
+
+
+ostream& operator<<(ostream& os, const Component& c)
+{
+    os << c.type << ':' << c.name << ':' << c.nA << ':' << c.nB << ':' << c.value << endl;
+    return os;
+}
+
+int main() {
+	string x;
+	vector<string> strings;
+	vector<Component> components;
+	while(getline(cin, x))
+	{
+		strings.push_back(x);
+	}
+
+	for(auto line : strings){
+		if(isComponent(line)){
+			string properties[4];
+			stringstream ss(line);
+			int count=0;
+			 while (ss >> properties[count]){
+					count++; }
+			string name;
+			if(isalnum((properties[0])[1])){name = properties[0];}
+			else{name =(properties[0]).substr (3,(properties[0].length())-1);}
+			Component c1 = {(properties[0])[0],name,properties[1],properties[2],stof(properties[3])};
+			components.push_back(c1);
+			cout << c1;
+		}
+		}
+
+
+	return 0;
+
+}
+
+
+
+
+
