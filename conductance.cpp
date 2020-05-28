be7999bad55bf6850ab99c6601e88ac38ce30c0f
David <dc2619@ic.ac.uk>
2020-05-28 17:05:00 +0200

    nothing of note

--- conductance.cpp
+++ conductance.cpp
@@ -3,10 +3,13 @@
 using namespace Eigen;
 using namespace std;
 
-/*/
+
 //move functions to header
 
 //how are variable sources implemented
+
+//create a new function that just updates the current vector
+/*/
 vector<pair<MatriXxd, vector<float>>> transient(vector<Components> comps, int noden, float length, float interval)
 {
     vector<pair<MatriXxd, vector<float>>> v;
@@ -29,18 +32,18 @@ int main()
     Node a {1, "a", 1};
     Node b {2, "b", 2};
     Node c {3, "c", 3};
-    Node d {4, "d", 4};
-    Node e {0, "e", 0};
+    Node d {0, "d", 0};
+    //Node e {0, "e", 0};
 
-    Component R1 {'R', "R1", a, b, 2};
-    Component V1 {'V', "V1", a, e, 5};
+    Component R1 {'R', "R1", a, c, 1};
+    Component V1 {'V', "V1", a, d, 5};
     Component R2 {'R', "R2", c, b, 3};
-    Component R4 {'R', "R4", e, d, 4};
-    Component I1 {'I', "I1", c, d, 6};
+    Component R3 {'R', "R3", b, d, 2};
+    //Component I1 {'I', "I1", c, d, 6};
 
-    vector<Component> comps {R1, V1, R2, R4, I1};
+    vector<Component> comps {R1, V1, R2, R3};
 
-    vector<Node> nodes {e, a, b, c, d};
+    vector<Node> nodes {d, a, b, c};
 
     int noden = compute_noden(nodes);
 
